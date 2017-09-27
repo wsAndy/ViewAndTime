cae1c45ab96d4c6f2a8d9f06fc9c9585c59db1ab
wsAndy <shengwangandy@foxmail.com>
2017-09-27 20:01:04 +0800

    change readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-ViewAndTime
+### ViewAndTime
 
-give out a demo
+
+##### How to use(in ubuntu)
+
+in ViewAndTime path,
+`$ cd build`
+`$ cmake ..`
+`$ make -j4`
--- build/CMakeFiles/main.dir/cmake_clean.cmake
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/main.dir/src/main.cpp.o"
-  "main.pdb"
-  "main"
+  "../main.pdb"
+  "../main"
 )
 
 # Per-language clean rules from dependency scanning.
